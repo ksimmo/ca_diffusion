6466834c8da16d7e8839804674ebf9cabc8fe248
Kim-Louis Simmoteit <kim-louis.simmoteit@iwr.uni-heidelberg.de>
2024-12-19 15:37:21 +0100

    adding EMA

--- ca_diffusion/models/autoencoder.py
+++ ca_diffusion/models/autoencoder.py
@@ -1,8 +1,13 @@
+import os
+from contextlib import contextmanager
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 import lightning.pytorch as pl
+
+from ca_diffusion.modules.ema import EMA
 
 #TODO: maybe put this in a separate file to support multiple distributions
 #Gaussian distribution used for VAE
@@ -47,7 +52,10 @@ class Autoencoder(pl.LightningModule):
                  image_mode: bool=True,
                  data_key: str="image",
                  ema_update_steps: int=-1,
-                 ignore_param: list=[]
+                 ema_smoothing_factor: float=0.999,
+                 ignore_param: list=[],
+                 ckpt_path: str=None,
+                 ignore_ckpt_keys: list=[]
                  ):
         super().__init__()
 
@@ -65,6 +73,49 @@ class Autoencoder(pl.LightningModule):
 
         #set up EMA if necessary
         self.use_ema = self.ema_update_steps>0
+        if self.use_ema:
+            self.encoder_ema = EMA(self.encoder, ema_smoothing_factor)
+            self.decoder_ema = EMA(self.decoder, ema_smoothing_factor)
+
+        if ckpt_path is not None and os.path.exists(ckpt_path):
+            self.init_from_ckpt(ckpt_path, ignore_ckpt_keys)
+
+    def init_from_ckpt(self, ckpt_path, ignore_keys: list=[]):
+        """
+        Initialize weights only from a checkpoint file 
+        """
+        sd = torch.load(ckpt_path, map_location="cpu", weights_only=True)["state_dict"]
+        for k in sd.keys():
+            for k2 in ignore_keys:
+                if k.startswith(k2):
+                    print("Removing key {} from state_dict".format(k))
+                    del sd[k]
+
+        missing, unexpected = self.load_state_dict(sd, strict=False)
+        if len(missing)>0:
+            print("Missing keys:", missing)
+        if len(unexpected>0):
+            print("Unexpected keys:", unexpected)
+
+    @contextmanager
+    def ema(self, desc=None):
+        if self.use_ema:
+            #save weights for later and use ema weights
+            self.encoder_ema.save(self.encoder)
+            self.encoder_ema.use_ema(self.encoder)
+            self.decoder_ema.save(self.decoder)
+            self.decoder_ema.use_ema(self.decoder)
+            if desc is not None:
+                print("{}: Entering EMA!".format(desc))
+
+        try:
+            yield None
+        finally:
+            if self.use_ema:
+                self.encoder_ema.backup(self.encoder)
+                self.decoder_ema.backup(self.decoder)
+                if desc is not None:
+                    print("{}: Leaving EMA!".format(desc))
 
     def encode(self, x):
         z = self.encoder(x)
@@ -99,14 +150,18 @@ class Autoencoder(pl.LightningModule):
         self.log_dict(log, prog_bar=True, logger=True, on_step=False, on_epoch=True)
     
     #uncomment to search for unused parameters or inspect gradients
-    """
-    def on_after_backward(self):
-        for name, param in self.named_parameters():
-            if param.grad is None:
-                print(name)
-            #else:
-            #    print(name, torch.mean(param.grad))
-    """
+
+    #def on_after_backward(self):
+    #    for name, param in self.named_parameters():
+    #        if param.grad is None:
+    #            print(name)
+
+    def on_before_zero_grad(self, **kwargs):
+        if self.use_ema:
+            if self.global_steps%self.ema_update_steps==0:
+                self.encoder_ema(self.encoder)
+                self.decoder_ema(self.decoder)
+
 
     def configure_optimizers(self):
         params = list(self.encoder.parameters()) + list(self.decoder.parameters())
@@ -124,24 +179,24 @@ class Autoencoder(pl.LightningModule):
     def log_images(self, batch, **kwargs):
         log = {}
         gt = batch[self.data_key]
+        with self.ema("Logging"): #use EMA weights for logging images
+            z = self.encode(gt)
 
-        z = self.encode(gt)
+            if self.image_mode:
+                samples = [gt.cpu().unsqueeze(2)]
+                for i in range(10):
+                    sample = self.decode(z, **self.decode_args)
+                    samples.append(sample.cpu().unsqueeze(2))
 
-        if self.image_mode:
-            samples = [gt.cpu().unsqueeze(2)]
-            for i in range(10):
+                samples.append(torch.mean(torch.cat(samples[1:], dim=2),dim=2).unsqueeze(2))
+                log["samples"] = torch.cat(samples, dim=2)
+            else:
                 sample = self.decode(z, **self.decode_args)
-                samples.append(sample.cpu().unsqueeze(2))
+                gt = torch.cat([torch.mean(gt, dim=2).unsqueeze(2), gt], dim=2).cpu()
+                sample = torch.cat([torch.mean(sample, dim=2).unsqueeze(2), sample], dim=2).cpu()
 
-            samples.append(torch.mean(torch.cat(samples[1:], dim=2),dim=2).unsqueeze(2))
-            log["samples"] = torch.cat(samples, dim=2)
-        else:
-            sample = self.decode(z, **self.decode_args)
-            gt = torch.cat([torch.mean(gt, dim=2).unsqueeze(2), gt], dim=2).cpu()
-            sample = torch.cat([torch.mean(sample, dim=2).unsqueeze(2), sample], dim=2).cpu()
-
-            T = min(16, gt.size(2))
-            log["samples"] = torch.cat([gt[:,:,:T], sample[:,:,:T]], dim=-2)
+                T = min(16, gt.size(2))
+                log["samples"] = torch.cat([gt[:,:,:T], sample[:,:,:T]], dim=-2)
         return log
     
 
@@ -176,6 +231,7 @@ class AutoencoderGAN(Autoencoder):
         self.clip_gradients(optim_g, gradient_clip_val=1.0, gradient_clip_algorithm="norm")
         if (batch_idx + 1) % 4 == 0: #do gradient accumulation
             optim_g.step()
+            #maybe run EMA manually here!!!
             optim_g.zero_grad()
 
         log["train/loss_ae_rec"] = loss_rec.item()
--- ca_diffusion/modules/ema.py
+++ ca_diffusion/modules/ema.py
@@ -1,13 +1,43 @@
 import torch
 import torch.nn as nn
 
-#TODO take care of DeepSpeed later!!!
+#TODO take care of DeepSpeed later!!! (clone does not work for deepspeed zero3)
 class EMA(nn.Module):
-    def __init__(self, model, decay: float=0.999):
+    def __init__(self, model, smoothing_factor: float=0.999):
         super().__init__()
-        if decay<0 or decay>1.0:
-            raise ValueError("Decay must be between 0 and 1")
-        self.decay = decay
+        if smoothing_factor<0 or smoothing_factor>1.0:
+            raise ValueError("Smoothing factor must be between 0 and 1")
+        self.register_buffer("smothing_factor", torch.ones(1)*smoothing_factor)
 
-    def forward(self):
-        pass
+        #create a copy of the model parameters
+        for n,p in model.named_parameters():
+            if p.requires_grad:
+                newname = n.replace(".", "")
+                self.register_buffer(newname, p.detach().clone().data) #register so we can store it
+
+        #a placeholder for the params we use for backupping the original weights
+        self.model_backup = []
+
+    def forward(self, model):
+        #update ema weights
+        for n,p in model.named_parameters():
+            if p.requires_grad:
+                newname = n.replace(".", "")
+                ema_param = self.get_buffer(newname)
+                ema_param.data.copy_(torch.lerp(ema_param, p.data, self.smoothing_factor))
+
+    def save(self, model):
+        #save the current model weights so we do not loose them
+        self.model_backup = [p.clone() for p in model.parameters()]
+
+    def use_ema(self, model):
+        #replace model weights with our ema weights
+        for n,p in model.named_parameters():
+            if p.requires_grad:
+                newname = n.replace(".", "")
+                p.data.copy_(self.get_buffer(newname))
+
+    def backup(self, model):
+        #put old weights back into place
+        for p_current, p_backup in zip(self.model_backup, model.parameters()):
+            p_current.data.copy_(p_backup)
